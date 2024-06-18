44897d3381d7161b00131732f417644c4f2d4bcf
Jabbo <software@webdings.de>
2024-06-18 08:46:13 +0200

    Fix bug in `crafted-ui-config` (#424)
    `crafted-ui-display-line-numbers` called
    `crafted-ui--update-line-numbers-display` before it was defined.
    
    This closes #423

---
+++ init.el
@@ -0,0 +1,17 @@
+;; This assumes you cloned Crafted Emacs in you home directory, if you
+;; didn't, make sure to update the path to correctly reflect the
+;; location on your system.
+(load "~/.emacs.d/modules/crafted-init-config")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(load-prefer-newer t t)
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
